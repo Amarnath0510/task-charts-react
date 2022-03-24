4ee8c4223f1c9270317d122c924c761f09629ba3
Amarnath0510 <amarmuthu007@gmail.com>
2022-03-24 18:38:20 +0530

    react-app finished

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Header from './component/header';
 import Header1 from './component/header2';
 import Header2 from './header3';
 import Header3 from './header4';
+import Sider from './sider';
 
 
 function App() {
   return (
     <div className="App">
+    
+  
+    
+  
    <div className='chart'>
    <div className='headers'>
    <Header/>
@@ -21,9 +26,10 @@ function App() {
    </div>
     
     <Chart />
+  
    </div>
-    
-   
+    <Sider/>
+  
  
     </div>
  
---
+++ src/sider.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Sider(){
+    return(
+        <div className="sider">
+        <h4>All</h4>
+        <p className="ratings">Ratings 4.3-4.9</p>
+        <p>Reviews  100-1500</p>
+       <p className="distance">Dist.Range 15 Kms</p>
+        
+        </div>
+
+        
+
+      
+    )
+}
+export default Sider;
